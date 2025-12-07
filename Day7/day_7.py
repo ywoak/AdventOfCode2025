19a523b11f313c9585f73e743e107d3e39ba62c0
Antoine-dev <aleduc@student.42.fr>
2025-12-07 16:49:07 +0100

    Day7 Part 2 Done for the test, optimization issue for the real input, cant just visually flood fill

--- Day7/day_7.py
+++ Day7/day_7.py
@@ -1,5 +1,8 @@
 from collections import deque
 
+from IPython import embed
+import pdb
+
 from pprint import pprint as p
 import sys
 
@@ -13,7 +16,7 @@ def find_start(manifold: Manifold) -> Pos:
             return 0, y
     raise RuntimeError
 
-def tachyon_travel(manifold: Manifold, start: Pos, H: int):
+def tachyon_travel(manifold: Manifold, start: Pos):
     H, W = len(manifold), len(manifold[0])
     dist: int = 0
     split: int = 0
@@ -48,17 +51,60 @@ def tachyon_travel(manifold: Manifold, start: Pos, H: int):
 
     return split
 
-def part1(data: list[str]) -> int:
-    H = len(data)
+def part1(manifold: Manifold) -> int:
     #p(manifold := [[char for char in line] for line in manifold])
-    manifold: Manifold = [[char for char in line] for line in data]
     start = find_start(manifold)
-    return tachyon_travel(manifold, start, H)
+    return tachyon_travel(manifold, start)
 
-def part2():
-    pass
+def in_bound(H: int, W: int, x: int, y: int) -> bool:
+    return 0 <= x < H and 0 <= y < W
+
+def quantum_tachyon_travel(
+    manifold: list[list[str]],
+    current_pos: tuple[int, int],
+    H: int,
+    W: int,
+    beam_present: set[tuple[int, int]],
+    paths: int
+) -> int:
+
+    x, y = current_pos
+
+    # Sort de l’espace : on termine un chemin
+    if not in_bound(H, W, x, y):
+        return paths + 1
+
+    # Boucle : on ne revisite pas un nœud déjà pris dans ce faisceau
+    if (x, y) in beam_present:
+        return paths
+
+    # Nouvel ensemble pour éviter la mutation globale
+    new_beam = beam_present | {(x, y)}
+
+    # Si c’est un split '^'
+    if manifold[x][y] == '^':
+        for dy in [-1, +1]:
+            ny = y + dy
+            paths = quantum_tachyon_travel(
+                manifold, (x, ny), H, W, new_beam, paths
+            )
+
+    # Sinon on descend
+    else:
+        manifold[x][y] = '|'
+        paths = quantum_tachyon_travel(
+            manifold, (x + 1, y), H, W, new_beam, paths
+        )
+
+    return paths
+
+def part2(manifold: Manifold) -> int:
+    start = find_start(manifold)
+    paths: int = quantum_tachyon_travel(manifold, start, len(manifold), len(manifold[0]), set(), paths=0)
+    return paths
 
 if __name__ == '__main__':
-    manifold: list[str] = sys.stdin.read().splitlines()
+    data: list[str] = sys.stdin.read().splitlines()
+    manifold: Manifold = [[char for char in line] for line in data]
     print(f'We split {part1(manifold)} times')
-    #print(f'We split {part2(manifold)} times')
+    print(f'There is {part2(manifold)} path')
