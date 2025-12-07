0dcb7da052a938d1f2df4c0b58820bf5b5b6f610
Antoine-dev <aleduc@student.42.fr>
2025-12-07 04:06:40 +0100

    Updated the token retrieval to download the day input

--- Utils/get_input.py
+++ Utils/get_input.py
@@ -1,11 +1,18 @@
-import sys
+from dotenv import load_dotenv
 import requests
+import sys
+import os
+
+load_dotenv()
 
 num: str = sys.argv[1]
 input_url: str = f'https://adventofcode.com/2025/day/{num}/input'
 
 cookie_name: str = 'session'
-cookie_value: str = ''
+cookie_value: str | None = os.getenv('COOKIE_VALUE')
+
+if cookie_value is None:
+    raise RuntimeError('COOKIE_VALUE non défini, il vous manque le .env')
 
 r: requests.Response = requests.get(input_url, cookies={cookie_name: cookie_value})
 
