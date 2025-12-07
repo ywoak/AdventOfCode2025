8dbc70b60e1c74e6907e6f9b67ecc254ec0c4d64
Antoine-dev <aleduc@student.42.fr>
2025-12-07 04:27:43 +0100

    Improved and added types

--- Day6/day_6.py
+++ Day6/day_6.py
@@ -1,26 +1,27 @@
 import sys
 
+type Cephalopod_Problem = list[list[str]]
+type Cephalopod_Problems = list[Cephalopod_Problem]
 type Problem = tuple[str]
 type Problems = list[Problem]
-#type Cephalopod_Problems = list[list[list[Problem]]]
-type Cephalopod_Problems = list
 
-def is_not_empty(elem):
-    empty = True
+def is_not_empty(elem: list[str]) -> bool:
+    empty: bool = True
+    e: str
     for e in elem:
         if e.strip():
             empty = False
-            break
+            return not empty
     return not empty
 
 def get_cephalopod_data(data: str) -> Cephalopod_Problems:
-    a = list(map(list, zip(*[line for line in data.split('\n') if line])))
+    initial_zip: list[list[str]] = list(map(list, zip(*[line for line in data.split('\n') if line])))
 
-    res = []
-    one_problem = []
+    res: Cephalopod_Problems = []
+    one_problem: Cephalopod_Problem = []
 
-    length = len(a)
-    for i, elem in enumerate(a):
+    length = len(initial_zip)
+    for i, elem in enumerate(initial_zip):
         if is_not_empty(elem):
             one_problem.append(elem)
             if i == length - 1:
@@ -31,18 +32,20 @@ def get_cephalopod_data(data: str) -> Cephalopod_Problems:
 
     return res
 
-def format_cephalopod_expression(cephalopod_data):
-    sign = cephalopod_data[0].pop()
+def format_cephalopod_expression(cephalopod_data: Cephalopod_Problem) -> str :
+    sign: str = cephalopod_data[0].pop()
 
-    res = []
+    res: list[str] = []
+    operand_to_parse: list[str]
     for operand_to_parse in cephalopod_data:
-        s = ''
+        s: str = ''
+        elem: str
         for elem in operand_to_parse:
             if elem.strip():
                 s += elem
         res.append(s)
 
-    to_ret = sign.join(res)
+    to_ret: str = sign.join(res)
     return to_ret
 
 def get_data(data: str) -> Problems:
@@ -51,15 +54,15 @@ def get_data(data: str) -> Problems:
 def format_expression(problem: Problem):
     return problem[-1].join(problem[:-1])
 
-def part1(data: str):
+def part1(data: str) -> int:
     problems: Problems =  get_data(data)
     return sum(map(eval, map(format_expression, problems)))
 
-def part2(data: str):
+def part2(data: str) -> int:
     cephalopod_data: Cephalopod_Problems = get_cephalopod_data(data)
     return sum(map(eval, map(format_cephalopod_expression, cephalopod_data)))
 
 if __name__ == '__main__':
-    data = sys.stdin.read()
+    data: str = sys.stdin.read()
     print(f'Part 1 solution is {part1(data.strip())}')
     print(f'Part 2 solution is {part2(data)}')
