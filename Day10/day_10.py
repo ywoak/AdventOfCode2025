a37468ab7cd68266872faa77c8aba43cc2e8b759
Antoine-dev <aleduc@student.42.fr>
2025-12-10 06:13:41 +0100

    Boilerplate to start day 10

--- Day10/day_10.py
+++ Day10/day_10.py
@@ -8,6 +8,17 @@ type Coord = tuple[int, ...]
 type Coords = list[Coord]
 type Map = list[list[str]]
 
+def debug(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with: ")
+        p(args)
+        if kwargs: p(kwargs)
+        result = func(*args, **kwargs)
+        print(f"Function format {func.__name__} returned: {result}\n")
+        return result
+    return wrapper
+
+@debug
 def print_map(map: Map) -> None:
     for row in map:
         print("".join(row))
@@ -18,6 +29,7 @@ def f() -> None:
 def part1(map: Map) -> int:
     return 0
 
+@debug
 def format_input(data):#: Map):
     formatted = []
 
